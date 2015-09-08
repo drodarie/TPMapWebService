c8a478bef437daca7067693ba48a499b28589dd0
Dimitri <d.rodarie@gmail.com>
2015-09-08 09:28:37 +0200

    First commit

---
+++ src/calculette/Calculette.java
@@ -0,0 +1,45 @@
+package calculette;
+
+/**
+ * Created by Dimitri on 08/09/2015.
+ *
+ * @version 1.0
+ */
+public class Calculette {
+	/*
+	 * Addition
+	 */
+
+        public float getAddition(float opera, float operb) {
+            return opera + operb;
+
+        }
+
+	/*
+	 * Multiplication
+	 */
+
+        public float getMultiplication(float opera, float operb) {
+            return opera * operb;
+
+        }
+
+        /*
+         * Soustraction
+         */
+        public float getSoustraction(float opera, float operb) {
+            return opera - operb;
+
+        }
+
+        /*
+         * Division erreur non gérée par le web service
+         */
+        public float getDivision(float opera, float operb) {
+            if (operb != 0)
+                return opera / operb;
+            else
+                return 0;
+        }
+
+}
---
+++ src/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package example;/**
+ * Created by Dimitri on 08/09/2015.
+ * @version 1.0
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
